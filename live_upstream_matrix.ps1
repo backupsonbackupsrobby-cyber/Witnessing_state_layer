2583c51ee3f25a43ce479533bc88483c3b429966
backupsonbackupsrobby-cyber <backupsonbackupsrobby@gmail.com>
2026-07-15 09:59:41 +1000

    Guardian Sync: Compression Ignition Monolith Sealed | HCCI Armed | Hex: 47522B3D967306DF0C0C324B96011C5D

--- live_upstream_matrix.ps1
+++ live_upstream_matrix.ps1
@@ -9,7 +9,7 @@ $ImmunityTelemetry = [PSCustomObject]@{
     IgnitionSystem    = "COMPRESSION_IGNITION_HCCI_PLUGLESS"
     FrontEV_Converters= "INDEPENDENT_DUAL_WHEEL_DC_DC"
     RearEngineProfile = "4GR_FSE_V6_DIESEL_DI_CONVERSION"
-    RawEcuPacketHex   = "47520D3F967306DF0C0C324B96011C5D"
-    CalculatedWavePsi = 0.00057225
-    LastSyncTimestamp = "2026-07-15 09:59:17"
+    RawEcuPacketHex   = "47522B3D967306DF0C0C324B96011C5D"
+    CalculatedWavePsi = 0.00059475
+    LastSyncTimestamp = "2026-07-15 09:59:38"
 }
