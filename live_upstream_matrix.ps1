c769f37540e4fb80c74d0f3f7fccdb5804023d03
backupsonbackupsrobby-cyber <backupsonbackupsrobby@gmail.com>
2026-07-15 10:13:04 +1000

    Foundation Sync: Monolith Sealed | Cadence: V12 Rumble | Psi: 0.000615 | Hex: 47524648967302042812364E0C0C324B96

--- live_upstream_matrix.ps1
+++ live_upstream_matrix.ps1
@@ -11,12 +11,12 @@ $ImmunityTelemetry = [PSCustomObject]@{
     IgnitionSystem    = "ASYMMETRICAL_SPLIT_CYCLE_2_SPARK_4_HCCI"
     DrivetrainConfig  = "COMPRESSION_RELEASE_DISTRIBUTOR"
     FrontEV_Converters= "INDEPENDENT_DUAL_WHEEL_DC_DC"
-    FrontDiffDC_DC    = "ACTIVE_ENERGY_RECOVERY_43%"
-    RearDiffDC_DC     = "ACTIVE_ENERGY_RECOVERY_76%"
+    FrontDiffDC_DC    = "ACTIVE_ENERGY_RECOVERY_54%"
+    RearDiffDC_DC     = "ACTIVE_ENERGY_RECOVERY_78%"
     TelemetryMonitors = "6_HIGH_PRESSURE_DASH_GAUGES"
     IntakeDefense     = "DUAL_STEEL_WOOL_CATCH_CANS_ACTIVE"
     StructuralState   = "STABLE_PLANE"
-    RawEcuPacketHex   = "47520E419673020428122B4C0C0C324B96"
-    CalculatedWavePsi = 0.000573
-    LastSyncTimestamp = "2026-07-15 10:12:40"
+    RawEcuPacketHex   = "47524648967302042812364E0C0C324B96"
+    CalculatedWavePsi = 0.000615
+    LastSyncTimestamp = "2026-07-15 10:13:01"
 }
